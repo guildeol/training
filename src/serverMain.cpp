c4e4a831edc5f92c6304c471eccb7d8631c8920d
Guilherme <glhrmcosta91@gmail.com>
2015-04-15 17:19:25 -0300

    Initial test for server successful.

--- src/serverMain.cpp
+++ src/serverMain.cpp
@@ -29,11 +29,40 @@ int main(int argc, char *argv[])
     hints.ai_flags = AI_PASSIVE;
 
     server = new ServerSocket(NULL, port, hints, poolSize, backlog);
+
+    server->bind();
+    server->listen(backlog);
+
+    // O socket do servidor sempre e adicionado primeiro no array de descritores
+    server->add(server, POLLIN);
+
+    cout << "Aguardando conexões" << endl;
+
+    while (true)
+    {
+      server->poll(-1);
+
+      if (server->canRead(server))
+      {
+        newSocket = server->accept(poolSize);
+        server->add(newSocket, POLLIN|POLLOUT);
+
+        newSocket->send("Hello!\n");
+
+        break;
+      }
+    }
   }
   catch (exception e)
   {
     cout << e.what() << endl;
   }
+
+  if(server)
+    delete server;
+
+  if(newSocket)
+    delete newSocket;
 
   return 0;
 }
