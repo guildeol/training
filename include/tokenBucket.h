3e4782fc523a15808e538cd874fd334d2238d2c1
Guilherme <glhrmcosta91@gmail.com>
2015-04-23 10:31:33 -0300

    Added header file for token bucket implementation.

---
+++ include/tokenBucket.h
@@ -0,0 +1,51 @@
+/*!
+ * \file tokenBucket.h
+ * \brief Cabecalho para classe TokenBucket.
+ *
+ * \date 23/04/2015
+ * \author Guilherme Costa <glhrmcosta91@gmail.com>
+ */
+
+#ifndef TOKENBUCKET_H
+#define TOKENBUCKET_H
+
+/*!
+ * \class TokenBucket
+ * \brief Classe para implementacao do algoritmo token bucket de controle de
+ *        taxa de transferencia de dados.
+ */
+class TokenBucket
+{
+
+public:
+
+  /*!
+   * \brief Construtor da classe.
+   * \param[in] maxTokens Quantidade maxima de token que o balde suporta.
+   * \param[in] outRate taxa (em bytes/seg) de transferencia desejada.
+   * \param[in] refillRate Quantidade de tokens repostos por segundo.
+   */
+  TokenBucket(int maxTokens, int outRate, int refillRate);
+
+  /*!
+   * \brief Tenta consumir uma certa quantidade de tokens, caso não seja
+            possível, bloqueia.
+   * \param[in] amount Quantidade de tokens a ser consumida.
+   */
+  int consume(int amount);
+
+  /*!
+   * \brief Repoe refillRate tokens ao balde.
+   * \return Quantidade atual de tokens no balde.
+   */
+  int replenish();
+
+private:
+
+  int maxTokens = 0;
+  int outRate = 0;
+  int refillRate = 0;
+
+};
+
+#endif
